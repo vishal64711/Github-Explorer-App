048b76ff581ea480a428b593df00d8ed84d53fda
Vishal <vishal64711@gmail.com>
2024-02-11 11:25:00 +0530

    intergrated fetchUserdata with search component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,32 @@
 import React from 'react';
-import './App.css';
 import Search from './Components/Search';
 
 class App extends React.Component {
+
+  state = {
+    user: null,
+  }
+
+  fetchUserData = async username =>{
+    try{
+      const res = await fetch(`https://api.github.com/users/${username}`);
+      if(res.ok){
+        const data = await res.json();
+
+        return this.setState({
+          user: data,
+        })
+
+      }
+    }catch(err){
+      console.log(err);
+    }
+
+  }
+
   render(){
     return (
-      <Search/>
+      <Search fetchData = {this.fetchUserData} />
      );
   }
 }
--- src/Components/Search.js
+++ src/Components/Search.js
@@ -16,6 +16,7 @@ class Search extends React.Component {
 
     render() {
         const {username} = this.state;
+        const {fetchData} = this.props;
         return (
             <div className="bg-dark">
                 <div className="container py-5">
@@ -34,6 +35,7 @@ class Search extends React.Component {
                                 </div>
                                 <div className="col-3">
                                     <button
+                                        onClick={() => fetchData(username)}
                                         className="btn btn-large btn-success">
                                         Search
                                     </button>
