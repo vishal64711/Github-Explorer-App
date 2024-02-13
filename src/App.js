3c3f0ab4212d6c1733fa5123cbe9da940a10c46e
Vishal <vishal64711@gmail.com>
2024-02-13 14:09:44 +0530

    Implemented Pagination

--- src/App.js
+++ src/App.js
@@ -13,7 +13,6 @@ class App extends React.Component {
     userDataError: null,
     reposError: null,
     loading: false,
-    page: 1,
   }
 
   fetchUserData = async username => {
@@ -26,29 +25,27 @@ class App extends React.Component {
     return { error }
   }
 
-  fetchRepos = async username => {
-    const { page } = this.state
+  fetchRepos = async (username,page) => {
     const res = await fetch(`https://api.github.com/users/${username}/repos?page=${page}&per_page=${PAGE_SIZE}`);
     if (res.ok) {
       const data = await res.json()
-      return { data, page: page + 1 };
+      return { data};
     }
     const error = (await res.json()).message;
     return { error }
   }
 
   fetchData = async username => {
-    this.setState({ loading: true, userDataError: null, reposError: null, }, async () => {
+    this.setState({ loading: true}, async () => {
       try {
         const [user, repos] = await Promise.all([
           this.fetchUserData(username),
-          this.fetchRepos(username)
+          this.fetchRepos(username, 1)
         ])
         if (user.data !== undefined && repos.data !== undefined) {
           return this.setState({
             user: user.data,
             repos: repos.data,
-            page: repos.page,
             loading: false
           })
         }
@@ -66,46 +63,56 @@ class App extends React.Component {
     })
   }
 
-  loadMore = async()=>{
-    const{data, page} = await this.fetchRepos(this.state.user.login)
-    if(data) this.setState(state =>({
-      repos: [...state.repos, ...data],
+  loadPage = async (page) => {
+    const { data} = await this.fetchRepos(this.state.user.login, page)
+    if (data) this.setState(state => ({
+      repos: data,
       page,
-    }))
+    }));
   }
 
   render() {
-    const { 
-      userDataError, 
-      reposError, 
-      loading, 
-      user, 
+    const {
+      userDataError,
+      reposError,
+      loading,
+      user,
       repos,
       page
     } = this.state;
+
+    const renderRepos = !loading && !reposError && !!repos.length
+
     return (
       <div>
         <Search fetchData={this.fetchData} />
         <div className='container'>
           <div className='text-center pt-5'>
             {(loading && (<p>Loading....</p>))}
-            {userDataError && <p className='text-danger'> User:{userDataError}</p>}
-            {!loading && !userDataError && user && <UserCard user={user} />}
-            {!userDataError && reposError && <p className='text-danger'> {reposError} </p>}
-            {!loading &&
-              !reposError &&
-              repos.map(repo => <RepoCard key={(repo.id)} repo={repo} />)}
-
-            {!loading &&
-              !userDataError &&
-               user && 
-               (page - 1)*PAGE_SIZE < user.public_repos && (
-              <button className='btn btn-success' onClick={this.loadMore}>
-              Load More  
-              </button>  
-            ) }
-            
+            {userDataError && <p className='text-danger'> {userDataError}</p>}
           </div>
+          {!loading && !userDataError && user && <UserCard user={user} />}
+          {reposError && <p className='text-danger'> {reposError} </p>}
+
+          {renderRepos && (
+            <React.Fragment>
+              <div className='mb-4'>
+                {[...new Array(Math.ceil(user.public_repos / PAGE_SIZE))].map((_, index) => (
+                  <button 
+                   key={index} 
+                   className='btn btn-success'
+                   onClick={() => this.loadPage(index + 1)}>
+                    {index + 1}
+                  </button>
+                ),
+                )}
+              </div>
+
+              {repos.map(repo => (
+                <RepoCard key={repo.id} repo={repo}/>
+              ))}
+            </React.Fragment>
+          )}
         </div>
       </div>
       //we need to pass key value to distinguish from siblings
